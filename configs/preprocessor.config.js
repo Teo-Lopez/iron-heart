0a129ffa2f418d42a65d102fb8fb4dab5495ff66
Teo-Lopez <emaildeteo@gmail.com>
2021-04-07 21:19:04 +0200

    fixed path

--- configs/preprocessor.config.js
+++ configs/preprocessor.config.js
@@ -3,8 +3,9 @@ const path = require('path')
 module.exports = app =>
 	app.use(
 		require('node-sass-middleware')({
-			src: path.join(__dirname, 'public'),
-			dest: path.join(__dirname, 'public'),
+			src: path.join(__dirname, '..', 'public'),
+			dest: path.join(__dirname, '..', 'public'),
 			sourceMap: true,
+			force: true,
 		})
 	)
