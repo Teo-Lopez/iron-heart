85a1156b425bc7ce43b11a91596629c42499399b
Teo-Lopez <emaildeteo@gmail.com>
2021-04-07 21:19:41 +0200

    redone using service

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,59 +1,57 @@
 const apiService = new BodypartsService()
 
+function showLocations(elm, divId) {
+	let parag = document.createElement('p')
+
+	parag.innerHTML = elm.Name + `<img src="/images/link.png" alt="link image">`
+	parag.setAttribute('id', `${elm.ID}`)
+	parag.classList.add('symptom')
+	//LLAMADA A NUESTRA API BACK
+	parag.onclick = () => {
+		getSymptomsList(elm.ID)
+	}
+
+	document.getElementById(divId).appendChild(parag)
+	document.getElementById(divId).classList.remove('hidden')
+	document.getElementById(divId).classList.add('menu')
+}
+
+const getSymptomsList = id => {
+	apiService
+		.getDiagnosis(id)
+		.then(list => {
+			console.log(list)
+			list.forEach(data => {
+				const option = document.createElement('input')
+				option.setAttribute('type', 'checkbox')
+				option.setAttribute('value', `${data.ID}`)
+				option.setAttribute('name', `${data.Name}`)
+				option.setAttribute('class', 'col-md-6')
+
+				const label = document.createElement('label')
+				label.setAttribute('for', `${data.Name}`)
+				label.setAttribute('class', 'col-md-6')
+				label.innerHTML = data.Name
+				const li = document.createElement('li')
+				li.setAttribute('class', 'col-md-6')
+				li.append(label)
+				li.append(option)
+				document.getElementById('ulSymptoms').append(li)
+			})
+		})
+		.catch(err => console.log(err))
+}
+
 document.addEventListener(
 	'DOMContentLoaded',
 	() => {
-
-		const getSymptomsList = id => {
-			apiService
-				.getDiagnosis(id)
-				.then(list => {
-					console.log(list)
-					list.forEach(data => {
-						const option = document.createElement('input')
-						option.setAttribute('type', 'checkbox')
-						option.setAttribute('value', `${data.ID}`)
-						option.setAttribute('name', `${data.Name}`)
-						option.setAttribute('class', 'col-md-6')
-
-						const label = document.createElement('label')
-						label.setAttribute('for', `${data.Name}`)
-						label.setAttribute('class', 'col-md-6')
-						label.innerHTML = data.Name
-						const li = document.createElement('li')
-						li.setAttribute('class', 'col-md-6')
-						li.append(label)
-						li.append(option)
-						document.getElementById('ulSymptoms').append(li)
-					})
-				})
-				.catch(err => console.log(err))
-		}
-
 		//ARMS
-		document.getElementById('arms').onclick = e => {
+		document.getElementById('arms').onclick = _ => {
 			apiService
 				.getArmsLocations()
 				.then(fullList => {
 					document.getElementById('armsdiv').innerHTML = ''
-
-					fullList[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-
-						//LLAMADA A NUESTRA API BACK
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('armsdiv').appendChild(parag)
-					})
-					document.getElementById('armsdiv').classList.remove('hidden')
-					document.getElementById('armsdiv').classList.add('menu')
-
-					// document.querySelectorAll("sintoma").forEach(item => item.onclick = () => alert("HELLO"))
+					fullList[0].expanded.forEach(element => showLocations(element, 'armsdiv'))
 				})
 				.catch(err => console.log(err))
 		}
@@ -69,23 +67,10 @@ document.addEventListener(
 				.getAbdomenLocations()
 				.then(fullList => {
 					document.getElementById('abdomendiv').innerHTML = ''
-
-					fullList[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('abdomendiv').appendChild(parag)
-					})
-					document.getElementById('abdomendiv').classList.remove('hidden')
-					document.getElementById('abdomendiv').classList.add('menu')
+					fullList[0].expanded.forEach(element => showLocations(element, 'abdomendiv'))
 				})
 				.catch(err => console.log(err))
 		}
-
 		document.getElementById('abdomendiv').onclick = e => {
 			document.getElementById('abdomendiv').classList.add('hidden')
 			document.getElementById('abdomendiv').classList.remove('menu')
@@ -98,20 +83,7 @@ document.addEventListener(
 				.getChestLocations()
 				.then(fullList => {
 					document.getElementById('chestdiv').innerHTML = ''
-
-					fullList[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('chestdiv').appendChild(parag)
-					})
-
-					document.getElementById('chestdiv').classList.remove('hidden')
-					document.getElementById('chestdiv').classList.add('menu')
+					fullList[0].expanded.forEach(element => showLocations(element, 'chestdiv'))
 				})
 				.catch(err => console.log(err))
 		}
@@ -127,19 +99,7 @@ document.addEventListener(
 				.getHeadLocations()
 				.then(fullList => {
 					document.getElementById('headdiv').innerHTML = ''
-
-					fullList.data[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('headdiv').appendChild(parag)
-					})
-					document.getElementById('headdiv').classList.remove('hidden')
-					document.getElementById('headdiv').classList.add('menu')
+					fullList[0].expanded.forEach(element => showLocations(element, 'headdiv'))
 				})
 				.catch(err => console.log(err))
 		}
@@ -156,19 +116,7 @@ document.addEventListener(
 				.getLegLocations()
 				.then(fullList => {
 					document.getElementById('legsdiv').innerHTML = ''
-
-					fullList[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('legsdiv').appendChild(parag)
-					})
-					document.getElementById('legsdiv').classList.remove('hidden')
-					document.getElementById('legsdiv').classList.add('menu')
+					fullList[0].expanded.forEach(element => showLocations(element, 'legsdiv'))
 				})
 				.catch(err => console.log(err))
 		}
@@ -185,19 +133,7 @@ document.addEventListener(
 				.getSkinLocations()
 				.then(fullList => {
 					document.getElementById('skindiv').innerHTML = ''
-
-					fullList[0].expanded.forEach(element => {
-						let parag = document.createElement('p')
-						parag.innerHTML = element.Name + `<img src="/images/link.png" alt="">`
-						parag.setAttribute('id', `${element.ID}`)
-						parag.onclick = () => {
-							getSymptomsList(element.ID)
-						}
-
-						document.getElementById('skindiv').appendChild(parag)
-					})
-					document.getElementById('skindiv').classList.remove('hidden')
-					document.getElementById('skindiv').classList.add('menu')
+					fullList[0].expanded.forEach(element => showLocations(element, 'skindiv'))
 				})
 				.catch(err => console.log(err))
 		}
